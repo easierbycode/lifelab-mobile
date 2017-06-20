eca09f1c22bf8ebe93469503cb1f1619b84b54a8
Daniel Johnson <navseeker7@gmail.com>
2017-06-19 18:04:34 -0700

    sync up

--- config/copy.config.js
+++ config/copy.config.js
@@ -1,11 +1,13 @@
 const orgCopyConfig = require('@ionic/app-scripts/config/copy.config');
 
 
-orgCopyConfig.include.push(
-  {
-    src: 'node_modules/angularfire2/node_modules/firebase/firebase.js',
-    dest: 'www/assets/firebase.js'
-  }
-);
+// orgCopyConfig.include.push(
+//   {
+//     src: 'node_modules/angularfire2/node_modules/firebase/firebase.js',
+//     dest: 'www/assets/firebase.js'
+//   }
+// );
+
+orgCopyConfig.copyAssets.src.push( '{{ROOT}}/node_modules/angularfire2/node_modules/firebase/firebase.js' )
 
 module.exports = orgCopyConfig;
